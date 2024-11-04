a9222429d90e0d26f38d52f48890dbd6631eb8dc
Vasi-Cordea <150058199+Vasi-Cordea@users.noreply.github.com>
2024-11-04 15:24:53 +0200

    basic python code

---
+++ tests/PythonBasics.py
@@ -0,0 +1,31 @@
+print("Hello World")
+# comments in python
+
+#variables, no need to specify type
+a = 3
+
+Str = "Hello World"
+print(Str)
+
+b, c, d = 5, 4.7, "Great"
+print(a, b, c, d)
+
+#print ("value is"+b+"bfd")
+
+print(type(b))
+
+values = [1, 2, "Vasi", 4, 5]
+
+print(values[3]) #4
+
+print(values[-1]) #5
+print(values[1:3]) #2 vasi 4
+
+values.insert(6, "Adi")
+print(values)
+              #values.pop()
+values [0 ] = "Ella"
+print(values)
+
+
+
