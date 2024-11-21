3f3d594f86bcaec1e9424489b9dd37b82e224b7c
Vasi-Cordea <150058199+Vasi-Cordea@users.noreply.github.com>
2024-11-21 13:58:09 +0200

    python operations llp

--- tests/loops.py
+++ tests/loops.py
@@ -18,6 +18,14 @@ for i in obj:
 # sum of First Natural numbers 1+2+3+4+5 =15
 #range(i,j) -> i to j-1
 summation = 0
-for j in range(1, 6):
+for j in range(1, 6):   #for (i=0;1<5;i+2)
     summation = summation + j
 print(summation)
+
+print("*************************")
+for k in range(1,10,2):
+    print(k)
+
+    print("*************SKIPPING FIRST INDEX************")
+for m in range(10):
+        print(m)
