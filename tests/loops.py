04ccf72fbf328b343574657c3d1ea7911f095221
Vasi-Cordea <150058199+Vasi-Cordea@users.noreply.github.com>
2024-11-21 11:30:10 +0200

    dateTime TESTS

---
+++ tests/loops.py
@@ -0,0 +1,23 @@
+greeting = "Good Morning"
+a = 4
+
+if greeting == "Good Morning":
+    print(" Condition matches")
+    print("second line")
+else:
+    print("Condition did not match")
+
+print("if else condition code is completed")
+
+#for loop
+
+obj= [2, 3, 4, 5, 8]
+for i in obj:
+    print(i*2)
+
+# sum of First Natural numbers 1+2+3+4+5 =15
+#range(i,j) -> i to j-1
+summation = 0
+for j in range(1, 6):
+    summation = summation + j
+    print(j)
